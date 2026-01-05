364e137fb9ddf7fcbe0f6be81296aa7a492b1187
NiazAhmedNayeem <niazahmed.net@gmail.com>
2026-01-05 22:06:04 +0600

    create code share route

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,5 @@ Route::get('/', [FrontendController::class, 'index'])->name('frontend.index');
 Route::get('/editor/{language}', [FrontendController::class, 'editor'])->name('frontend.editor');
 Route::post('/run-code', [FrontendController::class, 'runCode'])->name('frontend.runCode');
 
+Route::post('/share-code', [FrontendController::class, 'shareCode'])->name('frontend.shareCode');
+Route::get('/share/{token}', [FrontendController::class, 'openShared'])->name('frontend.openShared');
